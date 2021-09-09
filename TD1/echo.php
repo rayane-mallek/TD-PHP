b218bb806297d5fdc2dd40470e2b051dd22eb110
Rayane MALLEK <rayane.mallek@etu.umontpellier.fr>
2021-09-09 09:25:47 +0200

    Fin exercice 5

--- TD1/echo.php
+++ TD1/echo.php
@@ -1,23 +1,28 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title> Mon premier php </title>
-    </head>
-   
-    <body>
-        Voici le résultat du script PHP : 
-        <?php
-          // Ceci est un commentaire PHP sur une ligne
-          /* Ceci est le 2ème type de commentaire PHP
-          sur plusieurs lignes */
-           
-          // On met la chaine de caractères "hello" dans la variable 'texte'
-          // Les noms de variable commencent par $ en PHP
-          $texte = "hello world !";
+<?php 
 
-          // On écrit le contenu de la variable 'texte' dans la page Web
-          echo $texte;
-        ?>
-    </body>
-</html> 
+$voiture1 = array(
+    "marque" => "Renault",
+    "couleur" => "bleu",
+    "immatriculation" => "YYY"
+);
+
+$voiture2 = array(
+    "marque" => "Toyota",
+    "couleur" => "rouge",
+    "immatriculation" => "ZZZ"
+);
+
+$voitures = array($voiture1, $voiture2);
+
+var_dump($voitures);
+
+if (empty($voitures)) {
+    echo "Il n'y a pas de voiture";
+} else {
+    foreach($voitures as $voiture) {
+        echo "<p>Voiture $voiture[immatriculation] de marque $voiture[marque] (couleur $voiture[couleur])</p>";
+}
+}
+
+?>
+
