b69fcf40fb44287548b13d4bc205ed1552d708e5
Rayane MALLEK <rayane.mallek@etu.umontpellier.fr>
2021-09-09 09:46:11 +0200

    Fin exercice 8

---
+++ TD1/formulaireVoiture.php
@@ -0,0 +1,24 @@
+<form method="get" action="creerVoiture.php">
+  <fieldset>
+    <legend>Mon formulaire :</legend>
+    <p>
+      <label for="immat_id">Immatriculation</label> :
+      <input type="text" placeholder="256AB34" name="immatriculation" id="immat_id" required/>
+    </p>
+
+    <p>
+      <label for="marque_id">Marque</label> :
+      <input type="text" placeholder="Renault" name="marque" id="marque_id" required/>
+    </p>
+
+    <p>
+      <label for="couleur_id">Couleur</label> :
+      <input type="text" placeholder="Rouge" name="couleur" id="couleur_id" required/>
+    </p>
+
+    <p>
+      <input type="submit" value="Envoyer" />
+    </p>
+  </fieldset> 
+</form>
+
