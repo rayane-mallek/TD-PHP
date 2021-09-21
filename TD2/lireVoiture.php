cc15a05f6e4dae97a298884896bf7dcbc9f5006a
Rayane MALLEK <rayane.mallek@etu.umontpellier.fr>
2021-09-21 17:29:15 +0200

    Fin ex 7

--- TD2/lireVoiture.php
+++ TD2/lireVoiture.php
@@ -1,11 +1,19 @@
 <?php 
 	require_once 'Model.php';
+	require_once '/home/ann2/mallekr/public_html/PHP/TD1/Voiture.php';
 
 	$rep = Model::getPDO()->query("SELECT * FROM voiture");
-	$tab_objet = $rep->fetchAll(PDO::FETCH_OBJ);
+	//$tab_objet = $rep->fetchAll(PDO::FETCH_OBJ);
 
-	foreach($tab_objet as $clé) {
+	$rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+	$tab_voit = $rep->fetchAll();
+
+
+	/*foreach($tab_objet as $clé) {
 		echo $clé->immatriculation . " " . $clé->marque . " " . $clé->couleur . "<br />";
-	}
+	}*/
 
+	foreach($tab_voit as $voiture) {
+		echo $voiture->afficher() . "<br />";
+	}
 ?>
