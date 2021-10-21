43cb5b17bf00c686e5d7cff241678ce7a8e84841
Rayane MALLEK <rayane.mallek@etu.umontpellier.fr>
2021-10-21 10:09:35 +0200

    fin ex1 td6

--- controller/routeur.php
+++ controller/routeur.php
@@ -3,7 +3,11 @@
 require_once File::build_path(array("controller", "ControllerVoiture.php"));
 
 // On recupère l'action passée dans l'URL
-$action = $_GET['action'];
+if (!isset($_GET['action'])) {
+    $action = "readAll";
+} else {
+    $action = $_GET['action'];
+}
 // Appel de la méthode statique $action de ControllerVoiture
 ControllerVoiture::$action(); 
 ?>
