c27bd8a2a0cd8ac59b6b909764d56a016f50d93e
Rayane MALLEK <rayane.mallek@etu.umontpellier.fr>
2021-10-21 08:08:16 +0200

    Fin de l'exercice 8 TD5

--- view/view.php
+++ view/view.php
@@ -5,12 +5,24 @@
         <title><?php echo $pagetitle; ?></title>
     </head>
     <body>
+    <nav style="text-align: center;">
+        <a href="index.php?action=readAll">Accueil des voitures</a>
+        <a href="index.php?action=readAll&controller=utilisateur">Accueil des utilisateurs</a>
+        <a href="index.php?action=readAll&controller=trajet">Accueil des trajets</a>
+    </nav>
+    <br />
     <?php
         // Si $controleur='voiture' et $view='list',
         // alors $filepath="/chemin_du_site/view/voiture/list.php"
         $filepath = File::build_path(array("view", $controller, "$view.php"));
         require $filepath;
     ?>
+    <br />
+    <p style="border: 1px solid black;text-align:center;padding-right:1em;">
+        Site de covoiturage de Rayane
+    </p>
+
+
     </body>
 </html>
 
