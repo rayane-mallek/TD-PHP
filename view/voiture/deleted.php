a6a55c1736f25cd17952ab3e605f39351e280b35
Rayane MALLEK <rayane.mallek@etu.umontpellier.fr>
2021-10-28 08:26:14 +0200

    fin ex3 td6

--- view/voiture/deleted.php
+++ view/voiture/deleted.php
@@ -1,4 +1,4 @@
 <?php
-    echo '<p1>La voiture' . htmlentities($_GET['immatriculation']) . ' a bien été créée.</p>';
+    echo '<p1>La voiture' . htmlentities($_GET['immatriculation']) . ' a bien été supprimée.</p>';
     require File::build_path(array("view", "voiture", "list.php"));
 ?>
--- view/voiture/detail.php
+++ view/voiture/detail.php
@@ -1,4 +1,5 @@
 <?php
-echo '<p> Voiture d\'immatriculation ' . htmlentities($v->getImmatriculation()) . '.</p>';
+    echo '<p> Voiture d\'immatriculation ' . htmlentities($v->getImmatriculation()) . '.</p>';
+    echo '<a href="index.php?action=delete&immatriculation=' . htmlentities($v->getImmatriculation()) . '">Supprimer la voiture</a>';
 ?>
 
