f45e59680a510180db80e3517ed17284c7c54bba
Rayane MALLEK <rayane.mallek@etu.umontpellier.fr>
2021-10-28 08:32:03 +0200

    correction ex3

--- view/voiture/detail.php
+++ view/voiture/detail.php
@@ -1,5 +1,5 @@
 <?php
     echo '<p> Voiture d\'immatriculation ' . htmlentities($v->getImmatriculation()) . '.</p>';
-    echo '<a href="index.php?action=delete&immatriculation=' . htmlentities($v->getImmatriculation()) . '">Supprimer la voiture</a>';
+    echo '<a href="index.php?action=delete&immatriculation=' . rawurlencode($v->getImmatriculation()) . '">Supprimer la voiture</a>';
 ?>
 
